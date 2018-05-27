b581f24f4c41d5200c7dd4a54352b1877c24699f
Bill Orcutt <orcutt@gmail.com>
2018-05-27 11:50:42 -0700

    Bump version, upgrade electron to latest.

--- Cracked/Demos/sea-of-sines.js
+++ Cracked/Demos/sea-of-sines.js
@@ -1,24 +1,34 @@
-//how many sine waves
-var sines = 30;
+//create a stub with a compressor, reverb & an output
+__().compressor().reverb().out(0.5);
 
-//create a stub to connect everything to
-__().compressor().dac();
+//declare & assign some variables:
+//number of voices, between 6 & 24
+var voices = __.random(6,25),
+    //pointer to random function to save on typing
+    r = __.random,
+    //major scale
+    maj = __.scales("major");
 
-//for loop to create sines->lowpass->panner + an lfo to modulate the lowpass
-for(var i=0;i<sines;i++){
-  __().
-  		sine(__.pitch2freq(
-		  //random note from the wholetone scale
-    			__.scales("wholetone")[__.random(0,6)]+(__.random(5,10)*12)
-  			)
-    	).
-  		lowpass({frequency:100,id:"lp"+i,q:__.random(1,30)}).
-  		panner(__.random(-100,100)/100).
-	  	gain(1/sines).
-  		connect("compressor");
-  //lfo with a very low frequency
-  __().lfo({type:"sine",gain:1000,frequency:__.random(1,100)/5000}).connect("#lp"+i);
+//loop and create voices to connect to stub above
+for(var i=0;i<voices;i++) {
+  
+  //freq for this voice from the major scale
+  var freq = __.pitch2freq(maj[r(0,maj.length-1)] + (r(1,6)*12));
+  //direction to pan, L or R
+  var dir = r(0,100) > 50 ? 1 : -1;
+  //position the sound in the stereo field
+  dir = r(1,100)/100 * dir;
+  //type of oscillator- other options: square, sawtooth
+  var type = r(0,100) > 50 ? "sine" : "triangle";
+  
+  //lfo to modulate the frequency of the voice, then the voice itself (either sine or triangle), then gain, a panner and connect to the compressor defined above
+  __().lfo({frequency:r(0,100)/1000,gain:r(1,200)/70}).osc({type:type,frequency:freq}).gain({gain:0,id:i+"_voice"}).panner({pan:dir,id:i+"_panner"}).connect("compressor");
+  //add an additonal lfo to modulate the gain of the voice
+  __().lfo({modulates:"gain",frequency:(r(1,100))/1000,gain:r(1,100)/40}).connect("#"+i+"_voice");
+  //add one last lfo to modulate the voice's panner
+  __().lfo({modulates:"pan",type:"sine",frequency:r(40,100)/100,gain:1}).connect("#"+i+"_panner");
+  
 }  
 
-//start it
+//start the whole thing up
 __.play();
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ App version of ["I Dropped My Phone The Screen Cracked"](https://github.com/bill
 Download the app for [Mac OS](https://github.com/billorcutt/Cracked/releases/download/0.1.5/Cracked.dmg) or
 [Linux](https://github.com/billorcutt/Cracked/releases/download/0.1.5-Linux/Cracked-linux-x64.tar.gz).
 
-Or build it yourself using Node JS v7.9.0
+Or build it yourself using Node JS v8.9.3
 
 ```bash
 npm install
@@ -14,10 +14,10 @@ npm install
 ./node_modules/.bin/electron . 
 
 # to package for Mac
-./node_modules/electron-packager/cli.js . Cracked --platform=darwin --arch=x64 --version=1.7.5 --icon=./cracked.icns --overwrite
+./node_modules/electron-packager/cli.js . Cracked --platform=darwin --arch=x64 --icon=./cracked.icns --overwrite
 
 # to package for Linux
-./node_modules/electron-packager/cli.js . Cracked --platform=linux --arch=x64 --version=1.7.5 --icon=./cracked.icns --overwrite
+./node_modules/electron-packager/cli.js . Cracked --platform=linux --arch=x64 --icon=./cracked.icns --overwrite
 
 # build installer for Mac
 ./node_modules/electron-installer-dmg/bin/electron-installer-dmg.js --overwrite Cracked-darwin-x64/Cracked.app Cracked --out=Installers/
