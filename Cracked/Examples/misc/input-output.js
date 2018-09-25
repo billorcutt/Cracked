2a8c25e3cec15d0a14efb671ede9b55786353e51
Bill Orcutt <orcutt@gmail.com>
2018-09-25 10:45:37 -0700

    Version 0.1.11 - updating example to incorporate multi_out

--- Cracked/Examples/misc/input-output.js
+++ Cracked/Examples/misc/input-output.js
@@ -11,3 +11,13 @@ __().adc(0.25).delay({feedback:0}).dac(0.75);
 //*adc = analog to digital convertor, dac = digital to analog converter
 
 
+//there's also "out" object which is an alias for dac if you wanna avoid acronyms.
+__().adc(0.25).delay({feedback:0}).out(0.75);
+
+
+//there's also a multi out object that allows you to address individual outputs in a multichannel
+//sound interface. the argument for multi_out is the index of the channel, starting w zero.
+
+//here's a stereo example, accessing the first & second channels at 0 & 1.
+__().adc().delay({feedback:0}).multi_out(0);
+__().adc().delay({feedback:0}).multi_out(1);
